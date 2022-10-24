9e6d60c63970a8ab18f3a8759866e2467bec72c5
dendy <kostaref@yandex.ru>
2022-10-24 19:35:18 +0300

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,8 +15,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('get:validators')->daily()->withoutOverlapping();
-        $schedule->command('get:validators-events 103')->everyFiveMinutes()->withoutOverlapping();
+        //$schedule->command('get:validators')->daily()->withoutOverlapping();
+        //$schedule->command('get:validators-events 103')->everyFiveMinutes()->withoutOverlapping();
     }
 
     /**
